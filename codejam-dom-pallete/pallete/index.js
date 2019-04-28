ffffd0911c825fcd44edd7ad6ccba862f92c76e6
SADmammoth <lo.ma200018@gmail.com>
2019-04-28 15:57:29 +0300

    feat: swap tool; change move tool realization

--- codejam-dom-pallete/pallete/index.js
+++ codejam-dom-pallete/pallete/index.js
@@ -1,9 +1,19 @@
 let current_tool="none";
 let current_color="#c4c4c4c";
+let move = null;
 
 
 function change_tool(name, btn){
   event.stopPropagation();
+  foreach_class((i)=>i.style.userSelect= "auto", "*:not([class*='figure'])");
+  foreach_class((i)=>i.style.cursor = "default", '.figure', ".color");
+  switch(name){
+    case("transform"): foreach_class((i)=>i.style.cursor = "url('./assets/cursors/exchange-alt.svg'), pointer", '.figure');break; 
+    case("bucket"): foreach_class((i)=>i.style.cursor = "url('./assets/cursors/fill-drip.svg'), pointer", '.figure');break; 
+    case("pipette"): foreach_class((i)=>i.style.cursor = "url('./assets/cursors/eye-dropper.svg') -10 -10, pointer", '.figure', '.color');break; 
+    case("move"): document.addEventListener('click', func, false);foreach_class((i)=>i.style.cursor = "url('./assets/cursors/arrows-alt.svg'), pointer", ".figure");break;
+    case("swap"): document.addEventListener('click', func, false);foreach_class((i)=>i.style.cursor = "url('./assets/cursors/arrows-alt.svg'), pointer", ".figure");break;
+  }
   if(name === current_tool){
     btn.classList.remove("active-btn")
     current_tool = 'none';
@@ -12,11 +22,11 @@ function change_tool(name, btn){
   current_tool = name;
   if(document.getElementsByClassName('active-btn').length != 0)
     document.getElementsByClassName('active-btn')[0].classList.remove("active-btn");
-    if(name !== 'name')
+    if(name !== 'none')
       btn.classList.add("active-btn");
-    let pick = function(fig){
+  let pick = function(fig){
     change_current_color(fig.target.style.backgroundColor);
-    document.removeEventListener('click', pick, false);
+   document.removeEventListener('click', pick, false);
     change_tool('none');
   }
   if(name === 'pipette'){
@@ -26,23 +36,25 @@ function change_tool(name, btn){
     document.removeEventListener('click', pick, false);
   }
 }
+  function foreach_class(callback, ...args){
+    args.forEach((i)=>Array.from(document.querySelectorAll(i)).forEach(callback));
+  
+  }
+
+  function func(){
+    if(move){
+      stop_tracking(move);
+    }
+  }
 
 function on_click_fig(figure){
+  event.stopPropagation();
+  event.preventDefault();
   switch(current_tool){
     case "transform": figure.classList.toggle('circle');break;
-    case "bucket": figure.style = `background-color: ${current_color}`;break;
-  }
-}
-
-function on_drag(figure){
-  switch(current_tool){
-    case "move": track_mouse(figure); break;
-  }
-}
-
-function on_dragend(figure){
-  switch(current_tool){
-    case "move":stop_tracking(figure); break;
+    case "bucket": figure.style.backgroundColor= current_color;break;
+    case "move": move_fig(figure); break;
+    case "swap": move_fig(figure); break;
   }
 }
 
@@ -56,7 +68,39 @@ function change_current_color(color){
   }
 }
 
+function move_fig(figure){
+  if(move === null){
+    move = figure;
+    figure.style.zIndex="3";
+  }
+  else{
+    figure.style.zIndex="2";
+    if(current_tool === "swap" && figure !== move){
+      swap(figure, move); 
+      move= null;
+      return;
+    }
+    move= null;
+    stop_tracking(figure);
+  }
+}
+
+function swap(fig1, fig2){
+  let buf = {};
+  buf.left = fig1.getBoundingClientRect().left;
+  buf.top = fig1.getBoundingClientRect().top;
+ 
+  fig1.style.left = parseInt(fig1.style.left)+fig2.getBoundingClientRect().left-fig1.getBoundingClientRect().left+'px';
+ 
+  fig1.style.top = parseInt(fig1.style.top)+fig2.getBoundingClientRect().top-fig1.getBoundingClientRect().top+'px';
+  fig2.style.left = parseInt(fig2.style.left)+buf.left- fig2.getBoundingClientRect().left+'px';
+  fig2.style.top = parseInt(fig2.style.top)+buf.top- fig2.getBoundingClientRect().top+'px';
+}
+
 function track_mouse(fig){ 
+  if(current_tool === "move" && move){
+  event.stopPropagation();
+  event.preventDefault();
   if(Number.isNaN(fig.style.left) || !fig.style.left){
     fig.style.left = 0; fig.style.top = 0;
   }
@@ -64,17 +108,20 @@ function track_mouse(fig){
   fig.getBoundingClientRect().left + 'px';
   fig.style.top=  parseInt(fig.style.top) + event.clientY - parseInt(fig.getBoundingClientRect().height)/2 - 
   fig.getBoundingClientRect().top + 'px' ;
+  }
 }
 
 function stop_tracking(fig){
+  document.removeEventListener('click', func, false);
   fig.style.left = parseInt(fig.style.left) + event.clientX - parseInt(fig.getBoundingClientRect().width)/2 -
   fig.getBoundingClientRect().left + 'px';
   fig.style.top =  parseInt(fig.style.top) + event.clientY - parseInt(fig.getBoundingClientRect().height)/2 - 
   fig.getBoundingClientRect().top + 'px';
+  
 }
 
 function RGBtoHEX(str){
-  if(str.match(/#[0-9a-fA-F]{6}/)[0] === str){
+  if(str.match(/#[0-9a-fA-F]{6}/)){
     return str;
   }
   let sf = new String(str);
